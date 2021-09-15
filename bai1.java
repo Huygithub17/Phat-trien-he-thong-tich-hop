e3a68174091b177e87b7605b499b05ea0a10ef82
Huygithub17 <72064322+Huygithub17@users.noreply.github.com>
2021-09-15 15:01:47 +0700

    Bài 1 -  tuần 2

---
+++ bai1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);// tạo đối tượng cs để lấy dữ liệu.
+        String a; //khai báo chuỗi a
+        System.out.println("Nhap chuoi:");
+        a = sc.nextLine();
+        System.out.println("Chuyen thanh chu hoa: ");
+        System.out.println(a.toUpperCase());
+    }
+}
